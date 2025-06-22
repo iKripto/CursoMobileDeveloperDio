41a2b0e80f84ba7514cfe3f17f7388a82ea49584
iKripto <yagosiqueira450@gmail.com>
2025-06-22 11:17:17 -0300

    Slide Ambiente Node.JS

---
+++ 02-meu-primeiro-projeto/src/index.js
@@ -0,0 +1,16 @@
+console.log("Olá, mundo");
+console.log("Bruce Wayne.");
+// node --watch arquivo.js;
+// Serve para ficar atualizando e rodando o programa toda vez que apertar para salvar.
+
+// Comandos para navegação de diretórios:
+// del *                        (delete) deleta arquivos.
+// ls                           (list) lista arquivos no diretório.
+// cls                          (clear) limpa terminal.
+// cd                           (change directory) muda diretório.
+// mkdir                        (make directory) cria um novo diretório.
+
+// Comandos npm e node.
+// npm init -y                  inicia um projeto node.
+// node [arquivo]               roda um arquivo javascript.
+// node --watch [arquivo]       node fica vigiando o arquivo.
